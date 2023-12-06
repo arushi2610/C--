3a8e31b395a39ed2963196ac8e0bb78b2c7c22c1
Arushi Malhotra <arushimalhotra2610@gmail.com>
2023-12-06 23:20:45 +0530

    Update and rename bintodec.cpp to binary_to_decimal.cpp

--- binary_to_decimal.cpp
+++ binary_to_decimal.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main(){
-    int n, val =0, base =1;
+    int n, val = 0, base = 1;
     cin>>n;
     while (n>0)
     {
-        int digit = n %10;
-        val += (digit*base);
-        n/=10;
-        base*=2;
+        int digit = n % 10;
+        val += (digit * base);
+        n /= 10;
+        base *= 2;
     }
     cout<<val;
     return 0;
